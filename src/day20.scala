6a472207f1f7ed50260ccaaf9848b4055e31679a
Julien Richard-Foy <julien@richard-foy.fr>
2021-12-20 11:49:53 +0100

    Add code for day 20

---
+++ src/day20.scala
@@ -0,0 +1,114 @@
+// using scala 3.1.0
+
+package day20
+
+import scala.util.Using
+import scala.io.Source
+import scala.annotation.tailrec
+
+@main def part1(): Unit =
+  println(s"The solution is ${part1(readInput())}")
+
+@main def part2(): Unit =
+  println(s"The solution is ${part2(readInput())}")
+
+def readInput(): String =
+  Using.resource(Source.fromFile("input/day20"))(_.mkString)
+
+def part1(input: String): Int =
+  val (enhancer, image0) = parseEnhancerAndImage(input)
+  val image1 = enhancer.enhance(image0)
+  val image2 = enhancer.enhance(image1)
+  image2.countLitPixels()
+
+def parseEnhancerAndImage(input: String): (Enhancer, Image) =
+  val enhancerAndImage = input.split("\n\n")
+  val enhancer         = Enhancer.parse(enhancerAndImage(0))
+  val image            = Image.parse(enhancerAndImage(1))
+  (enhancer, image)
+
+enum Pixel:
+  case Lit, Dark
+
+object Pixel:
+
+  def parse(char: Char): Pixel =
+    char match
+      case '#' => Pixel.Lit
+      case '.' => Pixel.Dark
+
+end Pixel
+
+object Enhancer:
+
+  def parse(input: String): Enhancer =
+    Enhancer(input.map(Pixel.parse))
+
+end Enhancer
+
+class Enhancer(enhancementString: IndexedSeq[Pixel]):
+
+  def enhance(image: Image): Image =
+    val pixels =
+      for y <- -1 until (image.height + 1)
+      yield
+        for x <- -1 until (image.width + 1)
+        yield enhancementString(pixelValue(image, x, y))
+
+    val outboundsPixel =
+      val value = if image.outboundsPixel == Pixel.Dark then 0 else 511
+      enhancementString(value)
+
+    Image(pixels, outboundsPixel)
+  end enhance
+
+  private def pixelValue(image: Image, x: Int, y: Int): Int =
+    var result  = 0
+    var bitMask = 1
+    for
+      yy <- (y + 1) to (y - 1) by -1
+      xx <- (x + 1) to (x - 1) by -1
+    do
+      if image.pixel(xx, yy) == Pixel.Lit then
+        result = result | bitMask
+      end if
+      bitMask = bitMask << 1
+    result
+  end pixelValue
+
+end Enhancer
+
+class Image(pixels: IndexedSeq[IndexedSeq[Pixel]], val outboundsPixel: Pixel):
+  require(pixels.nonEmpty, "There must be at least one row")
+  require(pixels.map(_.length).distinct.size == 1, "All the rows must have the same length")
+
+  val height = pixels.length
+  val width = pixels(0).length
+
+  def pixel(x: Int, y: Int): Pixel =
+    if y < 0 || y >= height then outboundsPixel
+    else if x < 0 || x >= width then outboundsPixel
+    else pixels(y)(x)
+
+  def countLitPixels(): Int =
+    pixels.view.flatten.count(_ == Pixel.Lit)
+
+end Image
+
+object Image:
+  def parse(input: String): Image =
+    val pixels =
+      input
+        .linesIterator.map(line => line.map(Pixel.parse))
+        .toIndexedSeq
+    Image(
+      pixels,
+      outboundsPixel = Pixel.Dark
+    )
+
+def part2(input: String): Int =
+  val (enhancer, image) = parseEnhancerAndImage(input)
+  LazyList
+    .iterate(image)(enhancer.enhance)
+    .apply(50)
+    .countLitPixels()
